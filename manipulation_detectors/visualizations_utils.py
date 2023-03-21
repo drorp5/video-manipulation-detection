d3389934b687299c0b56d7383958a6ddcef54751
drorp <drorperi5@gmail.com>
2023-03-21 23:08:15 +0200

    use plotly for visualization

--- manipulation_detectors/visualizations_utils.py
+++ manipulation_detectors/visualizations_utils.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
 import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 sns.set_theme("paper", "white")
 
 detectors_names = ['ConstantMetadata', 'FrameID', 'Timestamp', 'MSE', 'Histogram', 'OpticalFlow']
@@ -12,23 +16,25 @@ results_plot_transformation = {'ConstantMetadata' : lambda x: 1-x, #TODO: delete
 
 def plot_results(results_df: pd.DataFrame):
     num_graphs = len(results_df.columns)
-    fig, axs = plt.subplots(num_graphs, 1, sharex=True)
-    ax_id = 0
+    fig = make_subplots(rows=num_graphs, cols=1, shared_xaxes=True)
+    ind = 1
     for name, res in results_df.iteritems():
-        if num_graphs > 1:
-            ax = axs[ax_id]
-        else:
-            ax = axs
         res = res[1:]
-
         if name in results_plot_transformation:
             res = res.apply(results_plot_transformation[name])
-        res.plot(ax=ax)
-        ax.grid(True)
-        ax.set_ylabel(name, rotation=0)
-        ax.yaxis.set_label_coords(-0.1, 0.5)
-        ax_id +=1
-    plt.show()
+        fig.add_trace(
+            go.Scatter(
+                       y=res.values, 
+                       name=name,
+                       mode='lines'),
+                       row=ind,
+                       col=1
+        )
+        # ax.grid(True)
+        # ax.set_ylabel(name, rotation=0)
+        # ax.yaxis.set_label_coords(-0.1, 0.5)
+        ind +=1
+    fig.show()
 
 if __name__ == "__main__":
     pkl_path = r'C:\Users\drorp\Desktop\University\Thesis\video-manipulation-detection\OUTPUT\faking_matlab_rec_3.pkl'
