38ce28fb5afbc137cb55fb39b34a00215bd286a6
drorp <drorperi5@gmail.com>
2023-09-01 09:11:13 +0300

    add utilities functions to analyze adaptive params

---
+++ adaptive_parameters/utils.py
@@ -0,0 +1,80 @@
+import json
+from pathlib import Path
+import re
+import numpy as np
+import pandas as pd
+from .exposure_time_detection import ExposureTimeChangeDetector, ExposureChangeDetectionResult, ExposureValidationStatus
+
+def read_exposure_data(adaptive_parameters_path: Path):
+    with open (adaptive_parameters_path, 'r') as f:
+        data = json.load(f)
+    frames_exposure_id = []
+    frames_exposure_time = []
+    for frame_id, frame_data in data.items():
+        try:
+            frames_exposure_id.append(int(frame_id.split('_')[1]))
+            frames_exposure_time.append(frame_data["exposure_us"])
+        except:
+            continue
+    frames_exposure_id = np.array(frames_exposure_id)
+    frames_exposure_time = np.array(frames_exposure_time)
+    return frames_exposure_id, frames_exposure_time
+
+def read_intensity_data(frames_dir: Path):
+    frame_intensity_path = frames_dir / f"averaged_intensities.txt"
+    with open(frame_intensity_path, 'r') as f:
+        intensities_txt = f.read()
+    result = re.findall(r"frame (\d+): (\d+\.\d+)", intensities_txt)
+
+    frames_intensity_id = []
+    frames_intensity = []
+    for frame_id, intensity in result:
+        frames_intensity_id.append(int(frame_id))
+        frames_intensity.append(float(intensity))
+
+    frames_intensity_id = np.array(frames_intensity_id)
+    frames_intensity = np.array(frames_intensity)
+    return frames_intensity_id, frames_intensity
+
+
+def read_matches_data(matches_path: Path, exposure_df: pd.DataFrame, intensity_df: pd.DataFrame) ->pd.DataFrame:
+    df = pd.read_csv(matches_path.as_posix())
+    frames_exposure_id = exposure_df.index.to_numpy()
+    frames_exposure_time = exposure_df['exposure'].to_numpy()
+    frames_intensity_id = intensity_df.index.to_numpy()
+    frames_intensity_value = intensity_df['intensity'].to_numpy()
+
+    manual_exposure_time_change_frames_inds =  np.searchsorted(frames_exposure_id, df['exposure_frame'])
+    df['exposure_diff'] = frames_exposure_time[manual_exposure_time_change_frames_inds] - frames_exposure_time[manual_exposure_time_change_frames_inds-1]
+
+    non_nan_indices = np.argwhere(~np.isnan(df['intensity_frame']))[:,0]
+    manual_intensity_change_frames_id_filtered = df['intensity_frame'][non_nan_indices]
+
+    manual_intensity_change_frames_inds =  np.searchsorted(frames_intensity_id, manual_intensity_change_frames_id_filtered)
+    manual_intensity_change_frames_values_filtered = frames_intensity_value[manual_intensity_change_frames_inds] - frames_intensity_value[manual_intensity_change_frames_inds-1]
+
+    manual_intensity_change_frames_values = np.zeros(len(df))
+    manual_intensity_change_frames_values[non_nan_indices] = manual_intensity_change_frames_values_filtered
+    manual_intensity_change_frames_values[manual_intensity_change_frames_values==0] = np.nan
+    df['intensity_diff'] = manual_intensity_change_frames_values
+
+    df['ratio'] = df['intensity_diff'] / df['exposure_diff']
+    df['offset'] = df['intensity_frame'] - df['exposure_frame']
+    df['intensity'] = frames_intensity_value[np.searchsorted(frames_intensity_id, df['intensity_frame'])-1]
+    df['exposure'] = frames_exposure_time[np.searchsorted(frames_exposure_id, df['exposure_frame'])-1]
+    return df
+
+def detect_changes(exposure_intensity_df):
+    detector = ExposureTimeChangeDetector()
+    changes_detected = {}
+    for frame_id, frame_data in exposure_intensity_df[3:].iterrows():
+        detection_result = detector.feed_frame(frame_id, frame_data['exposure'], frame_data['intensity'])
+        if detection_result is not None and detection_result.status != ExposureValidationStatus.EVALUATION:
+            changes_detected[detection_result.change.id] = detection_result
+    return changes_detected
+
+def set_detection_matches(manual_matches_df, changes_detected):
+    manual_changes_1_df = manual_matches_df.copy()
+    manual_changes_1_df['abs_exposure_diff'] = abs(manual_changes_1_df['exposure_diff'])
+    manual_changes_1_df['detected'] = [changes_detected[k].status.name if k in changes_detected else 'NOT OBSERVED' for k in manual_changes_1_df['exposure_frame']]
+    return manual_changes_1_df
