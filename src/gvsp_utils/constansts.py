7a2560ac919077530601821744ad94c6d86ddced
drorp <drorperi5@gmail.com>
2023-10-01 12:07:41 +0300

    refactor/deleted config file

--- src/gvsp_utils/constansts.py
+++ src/gvsp_utils/constansts.py
@@ -1,3 +1,4 @@
+import cv2
 from vimba import PixelFormat
 
 #TODO: check if better in somewhere else
@@ -7,3 +8,4 @@ GVSP_LEADER_LAYER = "GVSP_LEADER"
 GVSP_TRAILER_LAYER = "GVSP_TRAILER"
 
 INT_TO_PIXEL_FORMAT = {0x1080009: PixelFormat.BayerRG8}
+CV2_CONVERSIONS = {PixelFormat.BayerRG8: cv2.COLOR_BayerRG2RGB}
