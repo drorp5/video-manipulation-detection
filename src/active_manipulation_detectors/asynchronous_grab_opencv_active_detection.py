123b78be1200d01ed5e23785125cc6b647e1c3d2
Dror Peri <drorperi5@gmail.com>
2024-04-30 07:24:20 +0300

    added streaming scipt

---
+++ src/active_manipulation_detectors/asynchronous_grab_opencv_active_detection.py
@@ -0,0 +1,161 @@
+"""BSD 2-Clause License
+
+Copyright (c) 2019, Allied Vision Technologies GmbH
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+import sys
+from typing import Optional
+from vimba import *
+import argparse
+from argparse import Namespace
+
+from gige.handlers import ViewerHandler
+from active_manipulation_detectors.side_channel.varying_shape_handler import VaryingShapeHandler
+from active_manipulation_detectors.side_channel.data_generator import RandomBitsGeneratorRC4
+from active_manipulation_detectors.side_channel.validation import DataValidatorKSymbols
+
+
+def print_preamble():
+    print('///////////////////////////////////////////////////////')
+    print('/// Vimba API Asynchronous Grab with OpenCV Example ///')
+    print('///////////////////////////////////////////////////////\n')
+
+def print_usage():
+    print('Usage:')
+    print('    python asynchronous_grab_opencv.py [camera_id]')
+    print('    python asynchronous_grab_opencv.py [/h] [-h]')
+    print()
+    print('Parameters:')
+    print('    camera_id   ID of the camera to use (using first camera if not specified)')
+    print()
+
+def abort(reason: str, return_code: int = 1, usage: bool = False):
+    print(reason + '\n')
+
+    if usage:
+        print_usage()
+
+    sys.exit(return_code)
+
+def get_camera(camera_id: Optional[str] = None) -> Camera:
+    with Vimba.get_instance() as vimba:
+        if camera_id:
+            try:
+                return vimba.get_camera_by_id(camera_id)
+
+            except VimbaCameraError:
+                abort('Failed to access Camera \'{}\'. Abort.'.format(camera_id))
+
+        else:
+            cams = vimba.get_all_cameras()
+            if not cams:
+                abort('No Cameras accessible. Abort.')
+
+            return cams[0]
+
+def setup_camera(cam: Camera, fps_val: Optional[int] = None):
+    with cam:
+        # Enable auto exposure time setting if camera supports it
+        try:
+            cam.ExposureAuto.set('Continuous')
+        except (AttributeError, VimbaFeatureError):
+                pass
+        
+        # Enable white balancing if camera supports it
+        try:
+            cam.BalanceWhiteAuto.set('Continuous')
+
+        except (AttributeError, VimbaFeatureError):
+            pass
+
+        # Try to adjust GeV packet size. This Feature is only available for GigE - Cameras.
+        try:
+            cam.GVSPAdjustPacketSize.run()
+
+            while not cam.GVSPAdjustPacketSize.is_done():
+                pass
+
+        except (AttributeError, VimbaFeatureError):
+            pass
+
+        # Set constant frame rate if specified
+        if fps_val is not None:
+            try:
+                cam.TriggerMode.set("Off")
+                cam.AcquisitionFrameRateAbs.set(fps_val)
+            except (AttributeError, VimbaFeatureError):
+                pass
+
+        # Query available, open_cv compatible pixel formats
+        # prefer color formats over monochrome formats
+        cv_fmts = intersect_pixel_formats(cam.get_pixel_formats(), OPENCV_PIXEL_FORMATS)
+        color_fmts = intersect_pixel_formats(cv_fmts, COLOR_PIXEL_FORMATS)
+
+        if color_fmts:
+            seleted_frame_format = color_fmts[0]
+            cam.set_pixel_format(seleted_frame_format)            
+        else:
+            mono_fmts = intersect_pixel_formats(cv_fmts, MONO_PIXEL_FORMATS)
+            if mono_fmts:
+                seleted_frame_format = mono_fmts[0]
+                cam.set_pixel_format(seleted_frame_format)
+
+            else:
+                abort('Camera does not support a OpenCV compatible format natively. Abort.')
+
+def parse_args() -> Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--camera_id", help="camera ID for direct access")
+    parser.add_argument("--duration", help="duration of streaming [seconds]. -1 infinite", type=float, default=None)
+    parser.add_argument("--buffer_count", help="streaming buffer", type=int, default=1)
+    parser.add_argument("--fps", help="frame rate [frames per second]", type=int, default=10)
+    
+    args = parser.parse_args()
+    return args
+
+def main_script():
+    print_preamble()
+    args = parse_args()
+    args.duration = 5
+    start_async_grab(args)
+    
+def start_async_grab(args):
+    with Vimba.get_instance():
+        with get_camera(args.camera_id) as cam:
+            setup_camera(cam, fps_val=args.fps)
+                        
+            # handler = ViewerHandler()
+            random_bits_generator = RandomBitsGeneratorRC4(key=b'key', num_bits_per_iteration=2)
+            data_validator = DataValidatorKSymbols(bits_in_symbol=2, symbols_for_detection=2)
+            handler = VaryingShapeHandler(random_bits_generator=random_bits_generator, data_validator=data_validator, num_levels=4, increment=20)
+
+            # Start Streaming with a custom frames buffer
+            try:
+                cam.start_streaming(handler=handler, buffer_count=args.buffer_count)
+                handler.shutdown_event.wait(args.duration)
+            finally:
+                cam.stop_streaming()
+              
+if __name__ == '__main__':
+    main_script()
