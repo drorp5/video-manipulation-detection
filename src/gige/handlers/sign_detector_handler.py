5f0458f8858ad00a2723fef72b25ce833005d55c
Dror Peri <drorperi5@gmail.com>
2024-05-10 12:42:04 +0300

    added handler for viewing with sign detection

---
+++ src/gige/handlers/sign_detector_handler.py
@@ -0,0 +1,31 @@
+from typing import Optional
+import cv2
+import numpy as np
+from vimba import Camera, Frame
+
+from gige.handlers import ViewerHandler
+from sign_detectors.stop_sign_detectors import StopSignDetector, draw_bounding_boxes
+
+
+StopSignDetector
+
+
+ENTER_KEY_CODE = 13
+
+
+class SignDetectorHandler(ViewerHandler):
+    def __init__(self, downfactor: int = 4,  detector: Optional[StopSignDetector] = None):
+        super().__init__(downfactor=downfactor)
+        self.detector = detector
+    
+    def detect_objects_in_image(self, img: np.ndarray) -> np.ndarray:
+        detections = self.detector.detect(img)
+        img = draw_bounding_boxes(img, detections)
+        return img
+
+    def plot(self, img: np.ndarray, cam: Camera) -> None:
+        img = self.resize_image(img)
+        if self.detector is not None:
+            img = self.detect_objects_in_image(img)
+        window_name = f"Stream from '{cam.get_name()}'. Press <Enter> to stop stream."
+        cv2.imshow(window_name, img)
