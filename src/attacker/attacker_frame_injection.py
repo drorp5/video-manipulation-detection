22b5866c132ecbf4fa6893a40a57400cb184bb91
Dror Peri <drorperi5@gmail.com>
2024-06-02 00:32:16 +0300

    fixed bug ampiric frame rate for frame injection

--- src/attacker/attacker_frame_injection.py
+++ src/attacker/attacker_frame_injection.py
@@ -12,7 +12,7 @@ class GigEAttackerFrameInjection(GigEAttacker):
         self.gige_link.fake_still_image(
             img_path=self.config["injection"]["fake_path"],
             duration=time_left,
-            frame_rate=self.config["timing"]["ampiric_frame_time_in_seconds"],
+            frame_rate=1/self.config["timing"]["ampiric_frame_time_in_seconds"],
         )
 
     def run_attack_stage(self) -> None:
