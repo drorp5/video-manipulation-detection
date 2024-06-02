2dfca28354b7204a1bc66ac4ab4bf440c8dccff4
Dror Peri <drorperi5@gmail.com>
2024-06-02 11:58:13 +0300

    fixed frame injection attack bugs

--- src/attacker/attacker_frame_injection.py
+++ src/attacker/attacker_frame_injection.py
@@ -1,13 +1,13 @@
-from time import time
+import logging
 
 from attacker import GigEAttacker
 
 
 class GigEAttackerFrameInjection(GigEAttacker):
     def attack(self) -> None:
-        start_time = time()
         self.set_gige_link()
-        time_left = time() - start_time
+        time_left = self.config["timing"]["attack_duration_in_seconds"]
+        self.log(f"Attempting full frame injection for {time_left} seconds", logging.DEBUG)
         # TOOO: check if gvsp payload packets creation is time consuming and better off once
         self.gige_link.fake_still_image(
             img_path=self.config["injection"]["fake_path"],
