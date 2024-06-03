b52d2fc02d4332150ddacf2730a1c77874a7b823
drorp <drorperi5@gmail.com>
2024-06-03 14:35:01 +0300

    changed cleanup of delayed validator to acount for delay

--- src/active_manipulation_detectors/side_channel/validation/delayed_last_k_validator.py
+++ src/active_manipulation_detectors/side_channel/validation/delayed_last_k_validator.py
@@ -3,21 +3,40 @@ from active_manipulation_detectors.side_channel.validation import DataValidatorK
 
 
 class DataValidatorKSymbolsDelayed(DataValidatorKSymbols):
-    def __init__(self, bits_in_symbol: int, symbols_for_detection: int, max_delay: int = 0) -> None:
-        super().__init__(bits_in_symbol=bits_in_symbol, symbols_for_detection=symbols_for_detection)
+    def __init__(
+        self, bits_in_symbol: int, symbols_for_detection: int, max_delay: int = 0
+    ) -> None:
+        super().__init__(
+            bits_in_symbol=bits_in_symbol, symbols_for_detection=symbols_for_detection
+        )
         self.max_delay = max_delay
-        self.search_window = (max_delay+symbols_for_detection) * bits_in_symbol
+        self.search_window = (max_delay + symbols_for_detection) * bits_in_symbol
         self.detected_delay = None
 
-    def _validate(self) -> ValidationStatus:        
+    def _validate(self) -> ValidationStatus:
         if len(self.received_data) < self.search_window:
             return ValidationStatus.Incomplete
-        
-        target =  self.transmitted_data[:self.bits_for_detection]
-        for delay in range(self.max_delay+1):
-            delayed_received_pattern = self.received_data[delay*self.bits_in_symbol: delay*self.bits_in_symbol + self.bits_for_detection]
+
+        target = self.transmitted_data[: self.bits_for_detection]
+        for delay in range(self.max_delay + 1):
+            delayed_received_pattern = self.received_data[
+                delay * self.bits_in_symbol : delay * self.bits_in_symbol
+                + self.bits_for_detection
+            ]
             if delayed_received_pattern == target:
                 self.detected_delay = delay
                 return ValidationStatus.Valid
         self.detected_delay = None
         return ValidationStatus.Invalid
+
+    def clean(self, result: ValidationStatus) -> None:
+        if result == ValidationStatus.Incomplete:
+            return
+        self.transmitted_data = self.transmitted_data[self.bits_in_symbol :]
+
+        if self.detected_delay is None:
+            self.received_data = self.received_data[self.bits_in_symbol :]
+        else:
+            self.received_data = self.received_data[
+                self.detected_delay * self.bits_in_symbol :
+            ]
