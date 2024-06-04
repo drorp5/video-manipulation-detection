f28bbe3cc2fa3c8b97261e3924864d1391a9e254
Dror Peri <drorperi5@gmail.com>
2024-06-04 17:57:45 +0300

    changed frames statistics message

--- src/gige/frames_statistics.py
+++ src/gige/frames_statistics.py
@@ -8,11 +8,12 @@ class FramesStatistics:
     completed_frames: int
     partial_frames: int
 
+
     def pl(self) -> Optional[float]:
         try:
-            return 1 - self.completed_frames / self.total_frames
+            return 100 * (1 - self.completed_frames / self.total_frames)
         except ZeroDivisionError:
             return None
 
     def __str__(self) -> str:
-        return f"Total Frames = {self.total_frames}\nCompleted Frames = {self.completed_frames}\nPartial Frames = {self.partial_frames}\nPL = {self.pl()}"
+        return f"Total Frames = {self.total_frames}\nCompleted Frames = {self.completed_frames}\nPartial Frames = {self.partial_frames}\nPL = {self.pl():.1f}"
