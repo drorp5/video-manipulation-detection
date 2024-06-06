50aa8f41354baec0b7bff6df885c665ac7c4cf80
Dror Peri <drorperi5@gmail.com>
2024-06-06 11:00:43 +0300

    added log parser to extract widths values

--- src/driving_experiments/parsers.py
+++ src/driving_experiments/parsers.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 from pathlib import Path
 import re
+import pandas as pd
 
 
 def get_logged_frames_info(log_path: Path) -> Tuple[int, int, int]:
@@ -43,3 +44,47 @@ def summarize_log_file(log_path: Path) -> str:
     summary += f"Total {100*num_frames/(last_frame-first_frame+1):.1f}% logged\n"
     summary += f"Detections Frames = {detections_frames}"
     return summary
+
+
+def extract_frame_width_data(log_path: Path):
+    """
+    This function reads a log file and extracts frame number, transmitted width, and received value using regular expressions.
+
+    Args:
+        log_path: Path to the log file.
+
+    Returns:
+        A pandas DataFrame containing frame number, transmitted width, and received value.
+    """
+
+    data = []
+    transmitted_pattern = "Frame # (\d+): Setting next frame width = (\d+)"
+    received_pattern = "Frame # (\d+): (\d+) -> (\w+)"
+
+    prev_frame_num = None
+    with open(log_path.as_posix(), "r") as f:
+        for line in f:
+            matched_transmitted = re.search(transmitted_pattern, line)
+            match_received = re.search(received_pattern, line)
+            if matched_transmitted:
+                prev_frame_num, trasmitted_value = matched_transmitted.groups()
+                prev_frame_num = int(prev_frame_num)
+                trasmitted_value = int(trasmitted_value)
+            elif match_received:
+                frame_num, received_value, result = match_received.groups()
+                frame_num = int(frame_num)
+                received_value = int(received_value)
+
+                if frame_num == prev_frame_num + 1:
+                    data.append(
+                        {
+                            "frame_number": frame_num,
+                            "transmitted": trasmitted_value,
+                            "received": received_value,
+                            "result": result,
+                        }
+                    )
+
+    # Create pandas DataFrame from the extracted data
+    df = pd.DataFrame(data)
+    return df
